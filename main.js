20d13ed89e6b8a455d5335183e358cbe113b6280
Ahmedshafi1 <axmadshafi1@gmail.com>
2024-12-01 08:27:42 +0300

    assignmet js

---
+++ main.js
@@ -0,0 +1,22 @@
+// console 
+var name=prompt('Enter your Name: ')
+let phoneNumber=prompt('Enter your phone Number: ')
+let Address=prompt('Enter your Address: ')
+let Birth_day=prompt('Enter your  birth day: ')
+let age = 2024-Birth_day;
+console.log(name)
+console.log(phoneNumber)
+console.log(Address)
+console.log(Birth_day)
+
+console.log(age)
+
+document.write('<h1> Name:  '  , name ,'</h1> ')
+document.write( '<h3> phone_Numbe:  ' ,phoneNumber, '</P> ')
+document.write( '<h4> Address:' ,Address, '</h4>')
+document.write( '<h5> Birth_day:  ',Birth_day ,'</h5>')
+
+document.write( '<h6> age: ' ,age,'</h6>')
+
+
+
